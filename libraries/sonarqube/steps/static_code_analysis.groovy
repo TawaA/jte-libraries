bc66d4198d5928969c56800613b653e524d32f1a
TawaA <mobola86@gmail.com>
2021-08-12 13:12:03 -0400

    Update static_code_analysis.groovy

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,4 +1,4 @@
 void call(){
     println "sonarqube: static_code_analysis()"
-    println "num1 contains: ${pipelineConfig.num1} and num2 contains: ${pipelineConfig.num2}"
+    println "num1 contains: ${config.num1} and num2 contains: ${config.num2}"
 }
