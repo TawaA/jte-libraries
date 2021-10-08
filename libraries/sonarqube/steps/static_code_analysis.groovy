d4c7773c50e09fdf48048a01434c73beac29cb70
Tawa Alaka <mobola86@gmail.com>
2021-10-08 00:01:58 -0400

    Update static_code_analysis.groovy

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -2,6 +2,10 @@ void call(){
     //println "sonarqube: static_code_analysis()"
     echo"This is job branch ${pipelineConfig.jobBranch}"
     echo"This is short commit ${pipelineConfig.shortCommit}"
+    
+    config.docker_branch = pipelineConfig.jobBranch
+    echo"Printing docker branch ${config.docker_branch}"
+
     //make()
 }
 
