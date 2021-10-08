e138d944b9a809889d8634005e4ed8d5fd9b9619
Tawa Alaka <mobola86@gmail.com>
2021-10-08 00:31:29 -0400

    Update make_maven.groovy

--- libraries/maven/steps/make_maven.groovy
+++ libraries/maven/steps/make_maven.groovy
@@ -2,7 +2,6 @@ void call(){
     pipelineConfig.scmVars = checkout scm
     pipelineConfig.jobBranch = sh(script:"echo ${pipelineConfig.scmVars.GIT_BRANCH} | sed -e 's|origin/||g'",returnStdout:true).trim()
     pipelineConfig.shortCommit = sh(script:"git log -n 1 --pretty=format:'%h'",returnStdout:true).trim()
-    config.docker_branch = pipelineConfig.jobBranch
 
     echo"Printing Job branch ${pipelineConfig.jobBranch}"
     echo"Printing short commit ${pipelineConfig.shortCommit}"
